e7ec710c62dcf19f16d38be4e9233ea5efeaa242
khulanJargalsaikhan <onagraa@gmail.com>
2021-08-13 21:53:17 -0400

    third commit

--- src/main/java/com/example/springboot_exercise_401/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_exercise_401/SecurityConfiguration.java
@@ -18,8 +18,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/h2-console/**", "/").permitAll()
-                .antMatchers( "/register", "/updateTeam/**", "/deleteTeam/**").hasRole("ADMIN")
-                .antMatchers( "/addPlayer/**", "/addTeam/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers( "/register", "/updateTeam/**", "/deleteTeam/**", "/addTeam/**").hasRole("ADMIN")
+                .antMatchers( "/addPlayer/**", "/profile/**", "/deletePlayer/**", "/updatePlayer/**").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
